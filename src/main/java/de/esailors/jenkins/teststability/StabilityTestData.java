77480bd76d8f0df65c8e1d80d17fd2064ae47764
Christoph Kutzinski <kutzi@gmx.de>
2013-06-17 12:11:35 +0200

    Tweaked serialization format

--- src/main/java/de/esailors/jenkins/teststability/StabilityTestData.java
+++ src/main/java/de/esailors/jenkins/teststability/StabilityTestData.java
@@ -1,6 +1,5 @@
 package de.esailors.jenkins.teststability;
 
-import hudson.model.AbstractBuild;
 import hudson.tasks.junit.TestAction;
 import hudson.tasks.junit.TestObject;
 import hudson.tasks.junit.TestResultAction.Data;
@@ -10,12 +9,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+import jenkins.model.Jenkins;
+
 class StabilityTestData extends Data {
 	
 	private final Map<String,CircularBuffer> stability;
 	
-	private transient AbstractBuild<?,?> build;
-
 	public StabilityTestData(Map<String, CircularBuffer> stabilityHistory) {
 		this.stability = stabilityHistory;
 	}
@@ -36,14 +41,117 @@ class StabilityTestData extends Data {
 	
 	
 	public static class CircularBuffer {
-		  private boolean data[];
+		
+		static {
+			Jenkins.XSTREAM2.registerConverter(new ConverterImpl());
+		}
+		
+		public static class ConverterImpl implements Converter {
+
+			public boolean canConvert(@SuppressWarnings("rawtypes") Class type) {
+				return CircularBuffer.class.isAssignableFrom(type);
+			}
+
+			public void marshal(Object source, HierarchicalStreamWriter writer,
+					MarshallingContext context) {
+				CircularBuffer b = (CircularBuffer) source;
+				
+				writer.startNode("head");
+				writer.setValue(Integer.toString(b.head));
+				writer.endNode();
+				
+				writer.startNode("tail");
+				writer.setValue(Integer.toString(b.tail));
+				writer.endNode();
+
+				writer.startNode("size");
+				writer.setValue(Integer.toString(b.size));
+				writer.endNode();
+				
+				writer.startNode("data");
+				writer.setValue(dataToString(b.data));
+				writer.endNode();
+			}
+			
+			
+			private String dataToString(boolean[] data) {
+				StringBuilder buf = new StringBuilder();
+				for (boolean b : data) {
+					if (b) {
+						buf.append("1,");
+					} else {
+						buf.append("0,");
+					}
+				}
+				
+				if (buf.length() > 0) {
+					buf.deleteCharAt(buf.length() - 1);
+				}
+				
+				return buf.toString();
+			}
+
+			public CircularBuffer unmarshal(HierarchicalStreamReader r,
+					UnmarshallingContext context) {
+				
+				r.moveDown();
+				int head = Integer.parseInt(r.getValue());
+				r.moveUp();
+				
+				r.moveDown();
+				int tail = Integer.parseInt(r.getValue());
+				r.moveUp();
+				
+				r.moveDown();
+				int size = Integer.parseInt(r.getValue());
+				r.moveUp();
+				
+				r.moveDown();
+				String data = r.getValue();
+				r.moveUp();
+				
+				CircularBuffer buf = new CircularBuffer();
+				boolean[] b = stringToData(data);
+				
+				buf.data = b;
+				buf.head = head;
+				buf.size = size;
+				buf.tail = tail;
+				
+				return buf;
+			}
+			
+			private  boolean[] stringToData(String s) {
+				String[] split = s.split(",");
+				boolean d[] = new boolean[split.length];
+				
+				int i = 0;
+				for(String b : split) {
+					if ("1".equals(b)) {
+						d[i] = true;
+					} else if ("0".equals(b)) {
+						d[i] = false;
+					} else {
+						System.err.println("Invalid value: " + b); 
+					}
+					i++;
+				}
+				
+				return d;
+			}
+
+		}
+		
+		  private transient boolean data[];
 		  private int head; 
 		  private int tail;
 		  // number of elements in queue
 	      private int size = 0; 
 
-		  public CircularBuffer(int size) {
-		    data = new boolean[size];
+	      private CircularBuffer() {}
+	      
+		  public CircularBuffer(int maxSize) {
+		    data = new boolean[maxSize];
 		    head = 0;
 		    tail = 0;
 		  }
@@ -75,8 +183,7 @@ class StabilityTestData extends Data {
 		public boolean isEmpty() {
 			return data.length == 0;
 		}
-		
-		// TODO: minimal serialization format!
 	}
+	
 	
 }
